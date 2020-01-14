d0954ca9c06ccfc51ac8a6a15f1c35f667409f46
mwaurap <petermwaurawanjohi7@gmail.com>
2020-01-14 09:03:05 +0300

    css animation

--- main.js
+++ main.js
@@ -22,9 +22,27 @@ function initialize(countriesData) {
 
 function displayCountryInfo(countryByAlpha3Code) {
 const countryData = countries.find(country => country.alpha3Code === countryByAlpha3Code);
-console.log(countryData);
+// console.log(countryData);
 document.getElementById("capital").innerHTML = countryData.capital;
 document.querySelector("#flag-container img").src= countryData.flag;
 	document.querySelector("#countrycode ").placeholder = `+${countryData.callingCodes[0]}`;
 	document.getElementById("dialing-code").innerHTML = `+${countryData.callingCodes[0]}`;
+	document.getElementById("population").innerHTML = countryData.population;
+	document.getElementById("currencies").innerHTML = countryData.currencies.map(c => `${c.name} (${c.code})`);
+	document.getElementById("region").innerHTML = countryData.region;
+	document.getElementById("subregion").innerHTML = countryData.subregion;
 }
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(350,100);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(95,50,40,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(165, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.font = "30px Arial";
+ctx.strokeText("Hope you like the API", 10, 50);
